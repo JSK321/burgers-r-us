0a9886843821173a9bd4cc4114aa48309fbc28e4
Jae Kim <jaeshinkim321@gmail.com>
2020-10-15 14:55:06 -0700

    created seeds.sql, created database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon  Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Blue Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
